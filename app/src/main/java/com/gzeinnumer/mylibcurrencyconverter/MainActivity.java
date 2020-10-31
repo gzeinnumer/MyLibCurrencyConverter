909fc3736fae78fb76875b0cc16f59a855eaf1d1
Zein <m.fadli.zein.17@gmail.com>
2020-10-31 22:37:46 +0700

    first release

--- app/src/main/java/com/gzeinnumer/mylibcurrencyconverter/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibcurrencyconverter/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.gzeinnumer.mylibcurrencyconverter.utils.CurrencyConverter;
 
@@ -16,14 +17,16 @@ public class MainActivity extends AppCompatActivity {
 
     EditText editText;
     Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.ed);
-        button = findViewById(R.id.btn);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
 
 //        sample1();
         sample2();
@@ -37,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String str = editText.getText().toString();
                 Log.d(TAG, "onClick: "+CurrencyConverter.trimCommaOfString(str));
+
+                textView.setText(CurrencyConverter.trimCommaOfString(str));
             }
         });
     }
@@ -47,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
             public void realString(String value) {
                 Log.d(TAG, "realString: "+value);
                 Log.d(TAG, "realString: "+editText.getText().toString());
+
+                textView.setText("(Real Value) : "+value + " && (Preview) : "+editText.getText().toString());
             }
         }));
     }
